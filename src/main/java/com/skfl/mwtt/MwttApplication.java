8f746ed5146f9cb043b02bc29d898140cc3b94f7
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-07-17 21:23:17 +0400

    Intial commit

---
+++ src/main/java/com/skfl/mwtt/MwttApplication.java
@@ -0,0 +1,13 @@
+package com.skfl.mwtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skfl/mwtt/MwttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skfl.mwtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
