b85f84eef7bcb6197721da303ba0ffdf83b79723
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-07-17 22:02:04 +0400

    Add Counter service.

---
+++ src/main/java/com/skfl/mwtt/service/CounterService.java
@@ -0,0 +1,6 @@
+package com.skfl.mwtt.service;
+
+public interface CounterService {
+
+    int setValue(Long counterId, int value);
+}
---
+++ src/main/java/com/skfl/mwtt/service/impl/CounterServiceImpl.java
@@ -0,0 +1,26 @@
+package com.skfl.mwtt.service.impl;
+
+import com.skfl.mwtt.exception.CounterNotFoundException;
+import com.skfl.mwtt.repository.CounterRepository;
+import com.skfl.mwtt.service.CounterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CounterServiceImpl implements CounterService {
+
+    private final CounterRepository counterRepository;
+
+    @Override
+    @Transactional
+    public int setValue(Long counterId, int value) {
+        var counter = counterRepository.findById(counterId).orElseThrow(() -> {
+            throw new CounterNotFoundException("No Counter with such id were found");
+        });
+        counter.setValue(value);
+        counterRepository.saveAndFlush(counter);
+        return value;
+    }
+}
