0b0c41de0c9a8f6ff41bd3b852373c434fc9ec2c
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-07-18 16:26:57 +0400

    Add validation on val field

--- src/main/java/com/skfl/mwtt/model/entity/Counter.java
+++ src/main/java/com/skfl/mwtt/model/entity/Counter.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,6 +26,8 @@ public class Counter {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Min(value = Integer.MIN_VALUE)
+    @Max(value = Integer.MAX_VALUE)
     @Column(name = "val")
     private int val;
 
